4f96e635343debf2c77b113a8e20495fffe916b6
Cristian Espinoza Garner <crisgarner@pop-os.localdomain>
2018-12-29 17:21:27 -0600

    creates, updates and gets an user profile

--- contracts/Profiles.sol
+++ contracts/Profiles.sol
@@ -1,5 +1,61 @@
 pragma solidity ^0.5.0;
 
 contract Profiles {
-    
+
+    event LogCreateProfile(
+        address _owner,
+        bytes32 _name,
+        bytes32 _sex,
+        uint32 _age,
+        string _bio
+    );
+
+    event LogUpdateProfile(
+        address _owner,
+        bytes32 _name,
+        bytes32 _sex,
+        uint32 _age,
+        string _bio
+    );
+
+    struct Profile {
+        address owner;
+        bytes32 name;
+        bytes32 sex;
+        uint32 age;
+        string bio;
+    }
+
+    mapping(address => Profile) public addressToProfile;
+    address[] profilesArray;
+
+    function getCurrentProfilesSize() public view returns(uint){
+        return profilesArray.length;
+    }
+
+    function createProfile(
+        bytes32 _name,
+        bytes32 _sex,
+        uint32 _age,
+        string memory _bio
+    ) public {
+        require(addressToProfile[msg.sender].owner == address(0), "User already has an account");
+        Profile memory profile = Profile(msg.sender, _name, _sex, _age, _bio);
+        addressToProfile[msg.sender] = profile;
+        profilesArray.push(msg.sender);
+        emit LogCreateProfile(msg.sender,_name,_sex,_age,_bio);
+    }
+
+    function updateProfile(
+        bytes32 _name,
+        bytes32 _sex,
+        uint32 _age,
+        string memory _bio
+    ) public {
+        require(addressToProfile[msg.sender].owner != address(0), "User do not has an account");
+        Profile memory profile = Profile(msg.sender, _name, _sex, _age, _bio);
+        addressToProfile[msg.sender] = profile;
+        emit LogUpdateProfile(msg.sender,_name,_sex,_age,_bio);
+    }
+
 }
--- test/profiles.js
+++ test/profiles.js
@@ -1,7 +1,127 @@
+var BN = web3.utils.BN;
+require("chai").should();
+require("chai").use(require("chai-bignumber")(BN));
+
 const Profiles = artifacts.require("./Profiles.sol");
 
 contract("Profiles", accounts => {
-  it("...should store a profile.", async () => {
-    const instance = await Profiles.deployed();
+  beforeEach(async () => {
+    this.instance = await Profiles.deployed();
+  });
+
+  it("...should create an user profile.", async () => {
+    var receipt = await this.instance.createProfile(
+      web3.utils.utf8ToHex("Cristian Espinoza"),
+      web3.utils.utf8ToHex("Male"),
+      28,
+      "Building the Coffee Economy on the Blockchain @ Affogato Network.",
+      { from: accounts[1] }
+    );
+    receipt.logs.length.should.equal(1, "trigger one event");
+    receipt.logs[0].event.should.equal(
+      "LogCreateProfile",
+      "should be the LogCreateProfile event"
+    );
+    receipt.logs[0].args._owner.should.equal(
+      accounts[1],
+      "should equal to inserted"
+    );
+    expect(web3.utils.hexToUtf8(receipt.logs[0].args._name)).to.equal(
+      "Cristian Espinoza",
+      "should equal to inserted"
+    );
+    expect(web3.utils.hexToUtf8(receipt.logs[0].args._sex)).to.equal(
+      "Male",
+      "should equal to inserted"
+    );
+    receipt.logs[0].args._age
+      .toNumber()
+      .should.equal(28, "should equal to inserted");
+    receipt.logs[0].args._bio.should.equal(
+      "Building the Coffee Economy on the Blockchain @ Affogato Network.",
+      "should equal to inserted"
+    );
+
+    var revert = false;
+    try {
+      await this.instance.createProfile(
+        web3.utils.utf8ToHex("Eduardo Espinoza"),
+        web3.utils.utf8ToHex("Male"),
+        30,
+        "Building the Coffee Economy on the Blockchain @ Affogato Network.",
+        { from: accounts[1] }
+      );
+    } catch (err) {
+      revert = true;
+      assert(err.reason === "User already has an account");
+    }
+    expect(revert).to.equal(true, "Should revert on existing profile creation");
+    const size = await this.instance.getCurrentProfilesSize();
+    size.toNumber().should.be.equal(1);
+  });
+
+  it("...should get an user profile.", async () => {
+    var profile = await this.instance.addressToProfile(accounts[1]);
+    profile.owner.should.be.equal(accounts[1]);
+    expect(web3.utils.hexToUtf8(profile.name)).to.equal("Cristian Espinoza");
+    expect(web3.utils.hexToUtf8(profile.sex)).to.equal("Male");
+    profile.age.toNumber().should.be.equal(28);
+    profile.bio.should.be.equal(
+      "Building the Coffee Economy on the Blockchain @ Affogato Network."
+    );
+  });
+
+  it("...should update an user profile.", async () => {
+    var receipt = await this.instance.updateProfile(
+      web3.utils.utf8ToHex("Eduardo Espinoza"),
+      web3.utils.utf8ToHex("Male"),
+      32,
+      "Building the Coffee Economy on the Blockchain @ Affogato Network.",
+      { from: accounts[1] }
+    );
+    receipt.logs.length.should.equal(1, "trigger one event");
+    receipt.logs[0].event.should.equal(
+      "LogUpdateProfile",
+      "should be the LogUpdateProfile event"
+    );
+    receipt.logs[0].args._owner.should.equal(
+      accounts[1],
+      "should equal to updated"
+    );
+    expect(web3.utils.hexToUtf8(receipt.logs[0].args._name)).to.equal(
+      "Eduardo Espinoza",
+      "should equal to updated"
+    );
+    expect(web3.utils.hexToUtf8(receipt.logs[0].args._sex)).to.equal(
+      "Male",
+      "should equal to updated"
+    );
+    receipt.logs[0].args._age
+      .toNumber()
+      .should.equal(32, "should equal to updated");
+    receipt.logs[0].args._bio.should.equal(
+      "Building the Coffee Economy on the Blockchain @ Affogato Network.",
+      "should equal to updated"
+    );
+
+    var revert = false;
+    try {
+      await this.instance.updateProfile(
+        web3.utils.utf8ToHex("Eduardo Garner"),
+        web3.utils.utf8ToHex("Male"),
+        40,
+        "Building the Coffee Economy on the Blockchain @ Affogato Network.",
+        { from: accounts[5] }
+      );
+    } catch (err) {
+      revert = true;
+      assert(err.reason === "User do not has an account");
+    }
+    const size = await this.instance.getCurrentProfilesSize();
+    size.toNumber().should.be.equal(1);
+    expect(revert).to.equal(
+      true,
+      "Should revert on non existing profile update"
+    );
   });
 });
