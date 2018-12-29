7ae6fb798cfaa9729a934ee42cd2d7d369cd2560
Cristian Espinoza Garner <crisgarner@pop-os.localdomain>
2018-12-29 12:34:33 -0600

    initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Drizzle, generateStore } from "drizzle";
+import Profiles from "./contracts/Profiles.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// let drizzle know what contracts we want
+const options = { contracts: [Profiles] };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// setup the drizzle store and drizzle
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// pass in the drizzle instance
+ReactDOM.render(<App drizzle={drizzle} />, document.getElementById("root"));
+registerServiceWorker();
