2944b0025b95c91293ab17f4c5893d7e1b30f6bc
Cristian Espinoza Garner <ceegarner@gmail.com>
2019-01-11 13:09:57 -0600

    added navigation to app

--- client/src/profiles/Header.js
+++ client/src/profiles/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Nav, NavItem } from "reactstrap";
 import Blockies from "react-blockies";
+import { Link, Icon } from "rimble-ui";
 
 class Header extends Component {
   constructor(props) {
@@ -11,9 +12,41 @@ class Header extends Component {
     };
   }
 
+  componentDidMount() {
+    const { drizzle } = this.props;
+    this.checkOwner(drizzle);
+  }
+
+  async checkOwner(drizzle) {
+    const owner = await drizzle.contracts.Profiles.methods.owner().call();
+    var isOwner = owner == this.state.account ? true : false;
+    this.setState({ isOwner });
+  }
+
   render() {
     return (
       <Nav className="mt-4 justify-content-end">
+        <NavItem className="ml-2 mr-4 mt-4 pt-1 text-left ">
+          <Link href="/">
+            <Icon name="Home" size="20" className="mr-1" />
+            Profiles
+          </Link>
+        </NavItem>
+        <NavItem className="ml-2 mr-4 mt-4 pt-1 text-left ">
+          <Link href="/new">
+            <Icon name="PersonAdd" size="20" className="mr-1" />
+            New Profile
+          </Link>
+        </NavItem>
+
+        {this.state.isOwner && (
+          <NavItem className="ml-2 mr-4 mt-4 pt-1 text-left ">
+            <Link href="/admin">
+              <Icon name="Settings" size="20" className="mr-1" />
+              Admin Settings
+            </Link>
+          </NavItem>
+        )}
         <NavItem className="ml-2 mt-1 text-right ">
           <b>Current Account:</b> <br />
           <label>{this.state.account}</label>
